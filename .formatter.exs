08d5b854cd2ac340e6533eb2e1e9b64a25f7e87d
Pete Corey <petecorey@gmail.com>
2018-02-16 17:33:48 -0500

    Added mnemonic

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,5 @@
 # Used by "mix format"
 [
+  line_length: 80,
   inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
---
+++ lib/bip39/mnemonic.ex
@@ -0,0 +1,37 @@
+defmodule Bip39.Mnemonic do
+  def generate do
+    entropy
+    |> attach_checksum
+    |> map_onto_wordlist
+  end
+
+  defp entropy do
+    :crypto.rand_uniform(16, 32 + 1)
+    |> :crypto.strong_rand_bytes()
+  end
+
+  defp attach_checksum(entropy) do
+    hash = :crypto.hash(:sha256, entropy)
+
+    size =
+      entropy
+      |> bit_size
+      |> div(32)
+
+    <<checksum::bits-size(size), _::bits>> = hash
+
+    <<entropy::bits, checksum::bits>>
+  end
+
+  defp map_onto_wordlist(entropy) do
+    wordlist =
+      Application.fetch_env!(
+        :bip39_haiku,
+        :wordlist
+      )
+
+    for <<chunk::11 <- entropy>> do
+      Enum.at(wordlist, chunk)
+    end
+  end
+end
